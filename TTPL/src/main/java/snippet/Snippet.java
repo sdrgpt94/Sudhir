70e2a1f359ff38af0e0cffe5399f016cf64093e4
sdrgpt94 <124489560+sdrgpt94@users.noreply.github.com>
2023-02-05 13:45:36 +0530

    Add files via upload

---
+++ TTPL/src/main/java/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		startsWith.asArrayList("9","8")
+	}
+}
+
---
+++ TTPL/src/test/java/com/example/TTPL/MobileTest.java
@@ -0,0 +1,14 @@
+package com.example.TTPL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MobileTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
